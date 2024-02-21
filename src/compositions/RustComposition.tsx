eb30a97f00b765978f74368ccc611629dde3a1e5
Eray Erdin <eraygezer.94@gmail.com>
2024-02-21 13:30:12 +0300

    interpolate rotation

--- src/compositions/RustComposition.tsx
+++ src/compositions/RustComposition.tsx
@@ -3,15 +3,19 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { interpolate, useCurrentFrame } from "remotion";
 import { z } from "zod";
 import RustLogo from "../logos/Rust";
 
 export const rustCompSchema = z.object({})
 
 export const RustComposition = () => {
+  const frame = useCurrentFrame();
+  const degrees = interpolate(frame, [0, 60], [0, 45/4]);
+
   return (
     <div className="logo-container">
-      <RustLogo.Circle />
+      <RustLogo.Circle transform={`rotate(${degrees})`} />
       <RustLogo.Core />
     </div>
   )
