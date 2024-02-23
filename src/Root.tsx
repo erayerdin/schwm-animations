4f8ab72c350a0a7926fd963e87a6af6c135c506a
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-02-23 14:57:10 +0300

    Add Rust logo (#1)
    * add RustComposition
    
    * add rust logo component
    
    * add logo to RustComposition
    
    * separate rust logo to circle and core
    
    * fix naming
    
    * change fps to 60
    
    * interpolate rotation
    
    * allow capitalized comments
    
    * parametrize RustComposition
    
    * redefine prettier style
    
    * add prettier to workspace recommendations
    
    * configure prettier
    
    * rename id of rust logo composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,28 +1,28 @@
 // Copyright (c) 2024 Eray Erdin
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import { Composition } from 'remotion';
-import { MyComposition, myCompSchema } from './Composition';
+import {Composition} from 'remotion';
+import {RustComposition, rustCompSchema} from './compositions/RustComposition';
 import './style.css';
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
 			<Composition
-				id="MyComp"
-				component={MyComposition}
-				durationInFrames={240}
-				fps={30}
-				width={1280}
-				height={720}
-				schema={myCompSchema}
-				defaultProps={{
-					titleText: 'Welcome to Remotion with Tailwind CSS',
-					titleColor: '#000000',
-					logoColor: '#00bfff',
-				}}
+				id="RustLogoComposition"
+				component={RustComposition}
+				durationInFrames={60}
+				fps={60}
+				width={1080}
+				height={1080}
+				schema={rustCompSchema}
+				defaultProps={
+					{
+						// slowness: 8,
+					}
+				}
 			/>
 		</>
 	);
